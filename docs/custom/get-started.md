49a7144f239fe9d5326cf90170c12fcb70c4de53
TheLDB <ldb@erikboles.com>
2023-09-21 02:40:27 -0600

    add script

---
+++ docs/custom/get-started.md
@@ -0,0 +1,16 @@
+### Custom Get Started that does everything I need it to
+
+
+- Installs Docker
+- Installs NodeJS & NPM
+- Updates Node
+- Clones my custom Piston
+- Switches to development
+- Starts a dev server
+- Builds my custom package
+- Installs my custom package
+- Runs a test to ensure it worked
+
+```sh
+cd ~/piston/docs/custom && sudo chmod +x get-started.sh && sudo ./get-started.sh
+```
---
+++ docs/custom/get-started.sh
@@ -0,0 +1,27 @@
+# Install Docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+# Install NodeJS & NPM
+sudo apt update -y
+sudo apt install nodejs npm -y
+# Install NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+source ~/.bashrc
+# Install / Use latest version of Node
+nvm install node
+# Clone Piston Fork
+git clone https://github.com/TheLDB/piston
+cd piston/cli
+# Install CLI Deps
+npm i
+cd ..
+# Switch to dev environment
+./piston select dev
+# Start Server
+sudo ./piston start
+# Build Custom Package
+sudo ./piston build-pkg typescript farcaster
+# Install Custom Package
+./piston ppman install typescript=farcaster
+# Run Test
+./piston run typescript -l farcaster packages/typescript/farcaster/test.*
